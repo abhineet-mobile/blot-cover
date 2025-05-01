cec1941a184a77c24ee7c9e2b365cbb79527fb7c
Abhinav Thapa <avi.program@gmail.com>
2025-05-01 14:00:56 +0530

    Update wpconfig.php

--- wpconfig.php
+++ wpconfig.php
@@ -1,2 +1,2 @@
 //Edit WP.config file and add you API key there like this:
-define('BOLTCOVER_API_KEY', 'z9Zx4kjSim2bQPtNOnTyH3rgBUcnsfTa6ERCT7oh');
+define('BOLTCOVER_API_KEY', 'z9Zx4kjSi772bQPtNOnhgtTyH3rgBZwUcnsfT44a6ERCT7oh');
